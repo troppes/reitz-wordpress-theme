1d6fd74e31defb45873461a68cb9614c6a64112c
Florian Reitz <reitzf@hochschule-trier.de>
2020-01-11 22:43:11 +0100

    refac css and added comments

---
+++ reitz-theme/content-single.php
@@ -0,0 +1,7 @@
+<!-- loaded for every post -->
+<div class="row">
+    <h3><?php the_title(); ?></h3>
+    <p><?php the_date(); ?> by <?php the_author(); ?></p>
+    <?php the_content(); ?>
+</div>
+<hr>
---
+++ reitz-theme/single.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col s9 blog-main">
+            <?php
+            if (have_posts()) : while (have_posts()) : the_post();
+                get_template_part('content-single', get_post_format());
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+            endwhile; endif;
+            ?>
+        </div>
+        <div class="col s3 hide-on-small-and-down sidebar">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
