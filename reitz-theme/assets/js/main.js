39cae6d81271817a8bfe878347540079ff2e6c46
Florian Reitz <reitzf@hochschule-trier.de>
2020-01-11 23:32:28 +0100

    finished blog design

--- reitz-theme/assets/js/main.js
+++ reitz-theme/assets/js/main.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
     M.AutoInit();
 
+    // for archive sidebar
     $('#wordpress_archive').children().addClass('collection-item');
+
+
+    //For comment page
+    $('.comment-form-comment').addClass('input-field');
+    $('#comment').addClass('materialize-textarea');
+    $('#submit').addClass('waves-effect waves-light btn');
+    $('.avatar').addClass('circle responsive-img');
+    $('.comment-body').addClass('s12 card-panel grey lighten-5 ');
+    $('#reply-title').addClass('flow-text');
 });
--- reitz-theme/comments.php
+++ reitz-theme/comments.php
@@ -1,15 +1,15 @@
 <?php if ( post_password_required() ) {
     return;
 } ?>
-<div class="comments-area">
+<div class="comment-area">
     <?php if ( have_comments() ) : ?>
-        <h3 class="comments-title">
+        <h3 class="comment-title flow-text">
             <?php
             printf( _nx( 'One comment on "%2$s"', '%1$s comments on "%2$s"', get_comments_number(), 'comments title'),
                 number_format_i18n( get_comments_number() ), get_the_title() );
             ?>
         </h3>
-        <ul class="comment-list">
+        <ul class="comment-list container">
             <?php
             wp_list_comments( array(
                 'short_ping'  => true,
@@ -17,6 +17,11 @@
             ) );
             ?>
         </ul>
+    <?php endif; ?>
+    <?php if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+        <p class="no-comments">
+            <?php _e( 'Comments are closed.' ); ?>
+        </p>
     <?php endif; ?>
     <?php comment_form(); ?>
 </div>
